5f398ccfe34594c9b1e548fce3db0df963c3a0e4
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-05-08 13:26:52 +0530

    added authentication, authorization, server assets

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
 
 /*CONFIGURATIONS*/
@@ -33,8 +34,11 @@ const storage = multer.diskStorage({
 })
 const upload = multer({storage})
 
-// ROUTES WITH FILES
+/*ROUTES WITH FILES*/
 app.post("/auth/register", upload.single("picture"), register)
+
+/*ROUTES*/
+app.use("/auth", authRoutes)
 
 // MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
