a0fe13b20ce15fa52c9b31bf9ee4bf5804192fb9
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-06-19 00:05:13 +0530

    added post routes, rectified bugs, indents

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,10 +1,10 @@
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
-import User from "../models/user.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-// REGISTER USER
-export const register = async(req, res) => {
-  try{
+/* REGISTER USER */
+export const register = async (req, res) => {
+  try {
     const {
       firstName,
       lastName,
@@ -13,11 +13,11 @@ export const register = async(req, res) => {
       picturePath,
       friends,
       location,
-      occupation
-    } = req.body
-    
-    const salt = await bcrypt.genSalt()
-    const passwordHash = await bcrypt.hash(password, salt)
+      occupation,
+    } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
 
     const newUser = new User({
       firstName,
@@ -29,30 +29,29 @@ export const register = async(req, res) => {
       location,
       occupation,
       viewedProfile: Math.floor(Math.random() * 1000),
-      impressions: Math.floor(Math.random() * 1000)
-    })
-    const savedUser = await newUser.save()
-    res.status(201).json(savedUser)
-  }catch (err) {
-    res.status(500).json({error: err.message})
+      impressions: Math.floor(Math.random() * 1000),
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
 
-/*LOGGING IN*/
-export const login = async(req, res) => {
-  try{
-    const {email, password} = req.body
-    const user =await User.findOne({ email: email})
-    if(!user) return res.status(400).json({msg: "User does not exist"})
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist. " });
 
-    const isMatch = await bcrypt.compare(password, user.password)
-    if(!isMatch) return res.status(400).json({msg: "invalid credentials"})
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user._id}, process.env.JWT_SECRET)
-    delete user.password
-    res.status(200).json({ token, user })
-
-  }catch (err){
-    res.status(500).json({error: err.message})
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
   }
-}
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,18 +1,18 @@
-import express from "express"
-import{
+import express from "express";
+import {
   getUser,
   getUserFriends,
-  addRemoveFriends,
-} from "../controllers/users.js"
-import { verifyToken } from "../middleware/auth.js"
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
 
-const router = express.Router()
+const router = express.Router();
 
-/*READ*/
-router.get("/:id", verifyToken, getUser)
-router.get('/:id/friends', verifyToken, getUserFriends)
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
-/*UPDATE*/
-router.patch("/:id/:friendId", verifyToken, addRemoveFriends)
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
-export default router
+export default router;
