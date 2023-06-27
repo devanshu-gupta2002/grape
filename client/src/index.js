30fefec3b49417cb07bf4ce0366593e243e7ff9e
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-06-27 15:46:49 +0530

    cleared configureStore bug

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import authReducer from './state'
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 import {
   persistStore,
@@ -20,7 +20,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 const persistConfig = { key: "root", storage, version: 1}
 const persistedReducer = persistReducer(persistConfig, authReducer)
-const store = persistStore({
+const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => 
   getDefaultMiddleware({
