027726ed400d5f4874064bd21cbdc63be273b82d
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-06-27 23:10:56 +0530

    added themes

---
+++ client/src/components/flexBetween.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@mui/material";
+import styled from "@emotion/styled";
+
+const flexBetween = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center"
+})
+
+export default flexBetween;
