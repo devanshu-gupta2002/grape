2c10716ba1c13089f52693afddf81b918236c554
devanshu-gupta2002 <devanshuguptaknp@gmail.com>
2023-07-29 03:13:19 +0530

    completed login, register

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,33 @@
-const LoginPage = () => {
-  return(<div>loginpage</div>)
-}
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 
-export default LoginPage
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <Typography fontWeight="bold" fontSize="32px" color="purple">
+          🍇Grape
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
